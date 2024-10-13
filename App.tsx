b2baa88f9493d757511fef9e0591d38df60dad9a
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-13 17:32:00 -0600

    I wrapped the application in the PaperProvider

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import { SafeAreaView, StyleSheet} from 'react-native'
 import React from 'react'
 import HelloWorldScreen from './src/presentation/Screens/HelloWorldScreen'
 import CounterScreen from './src/presentation/Screens/CounterScreen'
+import { PaperProvider } from 'react-native-paper'
 
 export default function App() {
   return (
+    <PaperProvider>
     <SafeAreaView style={{flex:1}}>
      {/* <HelloWorldScreen name='Neftali Vergara'/> */}
     <CounterScreen/>
     </SafeAreaView>
+    </PaperProvider>
   )
 }
 
