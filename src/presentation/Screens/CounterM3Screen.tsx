2fb94f165e561380700ecbc0f33714d97cb9a349
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-18 12:48:07 -0600

    Add icon Ionic

--- src/presentation/Screens/CounterM3Screen.tsx
+++ src/presentation/Screens/CounterM3Screen.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { globalStyles } from '../theme/global.styles'
 import { FAB } from 'react-native-paper'
-
+import Icon from "react-native-vector-icons/Ionicons"
 
 export default function CounterM3Screen() {
 
@@ -11,6 +11,9 @@ export default function CounterM3Screen() {
   return (
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
+      <Text>Holaa</Text>
+
+    <Icon name="accessibility-outline" size={25} />
 
       <FAB
       label='+1'
