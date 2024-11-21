d4a19c5ecbd2726c7346166ca861cfd37fdb4c7d
Nefta11 <neftaliarturohernandez@gmail.com>
2024-11-21 11:02:01 -0600

    Agregar estilos y contenido al BoxObjectModelScreen

--- src/presentation/Screens/BoxObjectModelScreen.tsx
+++ src/presentation/Screens/BoxObjectModelScreen.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export const BoxObjectModelScreen = () => {
     return (
-        <View>
-            <Text>Box Object Model Screen</Text>
+        <View style={styles.container}>
+            {/*<Text style={styles.title}>Box Object Model Screen</Text>*/}
+            <View style={styles.purpleBox}>
+                <Text  style={{color:'white'}}>Hola Edmundo</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+    },
+    title: {
+        fontSize: 20,
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        borderWidth: 10,
+    },
+    purpleBox: {
+        height: 30,
+        backgroundColor: 'purple',
+        marginHorizontal: 20,
+        marginVertical: 50,
+        padding:5
+    }
+})  
