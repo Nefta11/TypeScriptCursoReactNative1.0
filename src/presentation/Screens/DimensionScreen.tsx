249365decea09fa3f259d82b0af67347ef5d88ea
Nefta11 <neftaliarturohernandez@gmail.com>
2024-11-28 11:21:59 -0600

    Actualizar DimensionScreen para usar useWindowDimensions y mejorar el estilo del texto

--- src/presentation/Screens/DimensionScreen.tsx
+++ src/presentation/Screens/DimensionScreen.tsx
@@ -1,15 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, useWindowDimensions } from 'react-native'
 import React from 'react'
 import { Dimensions } from 'react-native'
-const { width, height } = Dimensions.get('window')
+//const { width, height } = Dimensions.get('window')
 
 const DimensionScreen = () => {
+
+
+    const { width, height } = useWindowDimensions()
     return (
         <View>
             <View style={styles.container}>
-                <View style={styles.purpleBox} />
+                <View style={{
+                    ...styles.purpleBox,
+                    width: width * 0.6
+                }} />
             </View>
-            <Text> w:{width}, h:{height}</Text>
+            <Text style={styles.title}> w:{width}, h:{height}</Text>
         </View>
     )
 }
@@ -24,6 +30,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#5856D6',
         height: "50%",
         width: "50%"
+    },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: 'red'
     }
 })
 
