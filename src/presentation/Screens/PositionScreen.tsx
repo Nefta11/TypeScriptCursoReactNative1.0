a56d997a5b6c8f9870540618469daaafaf8e86f7
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-09 22:04:12 -0600

    Agregar PositionScreen y actualizar package.json para incluir react-native-maps

---
+++ src/presentation/Screens/PositionScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const PositionScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default PositionScreen
