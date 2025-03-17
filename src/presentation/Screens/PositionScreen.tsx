a535f839ea9f447f8311d1b909906d661ec5ee65
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-17 12:49:18 -0600

    Agregar estilos a PositionScreen y mejorar la estructura del componente

--- src/presentation/Screens/PositionScreen.tsx
+++ src/presentation/Screens/PositionScreen.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const PositionScreen = () => {
     return (
-        <View>
-            <Text>Holaa Mundo</Text>
+        <View style={styles.container}>
+            <Text
+            style={ styles.purpleBox }
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        backgroundColor: '#28c4d9',
+    },
+    purpleBox: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#5856d6',
+        borderWidth: 10,
+        borderColor: 'white',
+    }
+})
 
 export default PositionScreen
