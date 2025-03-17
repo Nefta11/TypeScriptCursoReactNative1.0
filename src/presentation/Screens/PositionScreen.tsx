54f988b0ebcabb9074fefcc1c3e74b756944fd14
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-17 12:53:53 -0600

    Agregar un nuevo cuadro naranja en PositionScreen y ajustar estilos

--- src/presentation/Screens/PositionScreen.tsx
+++ src/presentation/Screens/PositionScreen.tsx
@@ -3,14 +3,18 @@ import { View, Text, StyleSheet } from 'react-native'
 const PositionScreen = () => {
     return (
         <View style={styles.container}>
-            <Text
-            style={ styles.purpleBox }
+            <View
+                style={styles.purpleBox}
             />
+            <View
+                style={styles.orangeBox}
+            />
+
         </View>
     )
 }
 
-const styles = StyleSheet.create({ 
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#28c4d9',
@@ -19,6 +23,15 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         backgroundColor: '#5856d6',
+        borderWidth: 10,
+        borderColor: 'white',
+        position: 'relative',
+        top: 0,
+    },
+    orangeBox: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#f0a23b',
         borderWidth: 10,
         borderColor: 'white',
     }
